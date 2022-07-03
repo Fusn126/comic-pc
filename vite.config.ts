6928c99f3c50713aafcaf178d2d43cf54c3a2306
adicwu <youzaiwu@qq.com>
2022-07-03 11:08:00 +0800

    优化

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import viteCompression from 'vite-plugin-compression'
+import ElementPlus from 'unplugin-element-plus/vite'
 
 export default defineConfig({
   plugins: [
     vue(),
+    ElementPlus(),
     // 代码压缩
     viteCompression({
       verbose: true,
