0f398af9b927e6eaa8a4fcbb83e1e34cb2b2f07e
adicwu <youzaiwu@qq.com>
2022-07-11 17:56:23 +0800

    追番列表

--- src/layout/AppTabBar.vue
+++ src/layout/AppTabBar.vue
@@ -1,52 +1,13 @@
 <template>
   <div class="app-tab__bar">
-    <div v-click-outside="() => (historyVisible = false)" class="history">
+    <div v-click-outside="() => (comiclistVisible = false)" class="comiclist">
       <Icon
         class="tool"
-        :class="{ active: historyVisible }"
+        :class="{ active: comiclistVisible }"
         name="history"
-        @click="historyVisible = !historyVisible"
+        @click="comiclistVisible = !comiclistVisible"
       />
-      <transition
-        enter-active-class="animate__zoomIn"
-        leave-active-class="animate__hinge"
-      >
-        <div v-show="historyVisible" class="history-content">
-          <el-tabs v-model="tabs.active">
-            <el-tab-pane
-              v-for="item in tabs.list"
-              :key="item.value"
-              :label="item.name"
-              :name="item.value"
-            >
-              <div
-                v-for="k in playHistory.slice(0, 4)"
-                :key="k.id"
-                class="card"
-                @click="
-                  ;[toComicMain(k.id, 'replace'), (historyVisible = false)]
-                "
-              >
-                <BaseImg :src="k.cover" />
-                <div class="card-info">
-                  <p>{{ k.name }}</p>
-                  <span v-if="k.playEpisode">{{
-                    `观看至 ${k.playEpisode} ${k.playProgress}`
-                  }}</span>
-                  <span v-else class="bad">播放失败</span>
-                </div>
-              </div>
-              <div
-                v-if="playHistory.length >= 4"
-                class="more"
-                @click="$router.push({ name: 'User' })"
-              >
-                查看全部记录
-              </div>
-            </el-tab-pane>
-          </el-tabs>
-        </div>
-      </transition>
+      <AppTabBarComiclist v-model:visible="comiclistVisible" />
     </div>
     <div class="avatar">
       <BaseImg src="https://api.adicw.cn/uploads/UserAvatar/default.jpg" />
@@ -56,33 +17,16 @@
 
 <script lang="ts">
 import { computed, defineComponent, reactive, ref } from 'vue'
-import { usePlayCacheStore } from '@/stores/playCache.store'
-import { toComicMain } from '@/hooks/router'
+import AppTabBarComiclist from './AppTabBarComiclist.vue'
 export default defineComponent({
   name: 'AppTabBar',
+  components: {
+    AppTabBarComiclist
+  },
   setup() {
-    const playCacheStore = usePlayCacheStore()
-
-    const tabs = reactive({
-      active: '1',
-      list: [
-        // {
-        //   name: '收藏',
-        //   value: '0'
-        // },
-        {
-          name: '历史',
-          value: '1'
-        }
-      ]
-    })
-    const playHistory = computed(() => playCacheStore.playHistory)
-    const historyVisible = ref(false)
+    const comiclistVisible = ref(false)
     return {
-      tabs,
-      historyVisible,
-      playHistory,
-      toComicMain
+      comiclistVisible
     }
   }
 })
@@ -125,91 +69,9 @@ export default defineComponent({
       color: var(--primary-color);
     }
   }
-  .history {
+  .comiclist {
     position: relative;
     user-select: none;
-    &-content {
-      position: absolute;
-      right: -100%;
-      top: 100%;
-      width: 340px;
-      margin-top: 10px;
-      background: var(--aside-bg-color);
-      box-shadow: 0 0 10px rgb(0 0 0 / 0.2);
-      border-radius: 4px;
-      overflow: hidden;
-      animation-duration: 0.625s;
-      ::v-deep(.el-tabs) {
-        .el-tabs__header {
-          box-sizing: border-box;
-          padding: 0 16px;
-          .el-tabs__active-bar {
-            background-color: var(--warning-color);
-          }
-          .el-tabs__item {
-            color: var(--font-unactive-color);
-          }
-          .el-tabs__item.is-active,
-          .el-tabs__item:hover {
-            color: var(--font-color);
-          }
-        }
-        .el-tabs__content {
-          margin-bottom: 10px;
-          .el-tab-pane {
-            display: flex;
-            flex-direction: column;
-            gap: 14px;
-          }
-        }
-      }
-      .card {
-        display: flex;
-        width: 100%;
-        padding: 0 10px;
-        box-sizing: border-box;
-        cursor: pointer;
-        transition: all 0.25s;
-        img {
-          width: 100px;
-          aspect-ratio: 2/1.2;
-          border-radius: 4px;
-        }
-        &-info {
-          display: flex;
-          flex-direction: column;
-          justify-content: space-between;
-          flex: 1;
-          padding-left: 12px;
-          box-sizing: border-box;
-          font-size: 12px;
-          p {
-            .p-truncate(2);
-          }
-          span {
-            color: var(--font-unactive-color);
-            padding-bottom: 4px;
-          }
-          .bad {
-            color: var(--warning-color);
-            font-weight: 600;
-          }
-        }
-        &:hover {
-          filter: brightness(0.5);
-        }
-      }
-      .more {
-        font-size: 12px;
-        text-align: center;
-        cursor: pointer;
-        transition: all 0.25s;
-        &:hover {
-          text-decoration: underline;
-          opacity: 0.6;
-        }
-      }
-    }
   }
 }
 </style>
